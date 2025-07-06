2486535f29900959c9eb0641ffb02343844edc9f
sameera-g-mathad <sameeragmathad@gmail.com>
2025-07-05 21:02:54 -0500

    adding for continuity

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import { nt } from './ml-ts/numts';
+import { plotter } from './ml-ts';
 const Test: React.FC = () => {
-  // const a = nt.array([[25, 55, 1],
-  // [40, 85, 15],
-  // [22, 49, 0],
-  // [35, 75, 10],
-  // ]);
-  // console.log(nt.sqrt(64505185509.77778));
-  // console.log(nt.sqrt(70046841706.66667));
-  // console.log(nt.sqrt(66435686884.32099));
-  // console.log(nt.sqrt(46186954434.320984));
-
-  return <div></div>;
+  return <div className='plot-container'>sameer</div>;
 };
 
 Test.displayName = 'Test';
